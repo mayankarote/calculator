50365c71b58d71b113a589221669f8996f65ee19
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-09-25 12:32:00 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,11 @@
+class calculator {
+    constructor();
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')   
+const operationButtons = document.querySelectorAll('[data-operations]')   
+const equalsButtons = document.querySelector('[data-equals]')   
+const deleteButtons = document.querySelector('[data-delete]')   
+const allclearButtons = document.querySelector('[data-all-clear]')   
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')   
+const currentOperandTextElement = document.querySelector('[data-current-operand]')   
