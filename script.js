0178bc499da5683c941c148fc1a757322141c3a1
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-09-25 12:34:49 +0530

    Updated

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 class calculator {
-    constructor();
+  constructor(previousOperandTextElement, currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement;
+    this.currentOperandTextElement = currentOperandTextElement;
+  }
+
+  clear() {}
+  delete() {}
+  appendNumber() {}
+  chooseOperation() {}
+  compute() {}
+  updateDisplay() {}
 }
 
-const numberButtons = document.querySelectorAll('[data-number]')   
-const operationButtons = document.querySelectorAll('[data-operations]')   
-const equalsButtons = document.querySelector('[data-equals]')   
-const deleteButtons = document.querySelector('[data-delete]')   
-const allclearButtons = document.querySelector('[data-all-clear]')   
-const previousOperandTextElement = document.querySelector('[data-previous-operand]')   
-const currentOperandTextElement = document.querySelector('[data-current-operand]')   
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operations]");
+const equalsButtons = document.querySelector("[data-equals]");
+const deleteButtons = document.querySelector("[data-delete]");
+const allclearButtons = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+  "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+  "[data-current-operand]"
+);
